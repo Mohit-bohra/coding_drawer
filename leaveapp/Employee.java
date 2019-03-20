8e8e5e461a96af4de79a06446c86b12444c9df21
Mohit-bohra <37497565+Mohit-bohra@users.noreply.github.com>
2019-03-20 22:01:40 +0530

    Update Employee.java

--- leaveapp/Employee.java
+++ leaveapp/Employee.java
@@ -6,18 +6,23 @@ public class Employee implements Serializable {
 	private int empID;
 	private String name;
 	private int leaves;
-	private String reason;
-
-	Employee() 
-        {
-	};
-
-	Employee(int empID,String name, int leaves, String reason) {
+	private String reason;        
+	
+	Employee(int empID,String name, int leaves, String reason) 
+	  {
 		this.empID= empID;
 		this.name = name;
 		this.leaves = leaves;
 		this.reason = reason;
-	}
+	    }
+	   int getempID() 
+	   {
+           return empID;
+	   }
+           int getAvailableLeave() 
+	   {
+           return leaves;
+	   }
 
 	@Override
 	public String toString() {
